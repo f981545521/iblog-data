6e5c41cb8b7905f8aeafdc9502c108b7bec3e7ee
youfang <981545521@qq.com>
2018-02-27 18:33:06 +0800

    add : test cases

--- src/main/java/cn/acyou/iblogdata/conf/WebMvcConfig.java
+++ src/main/java/cn/acyou/iblogdata/conf/WebMvcConfig.java
@@ -13,7 +13,6 @@ public class WebMvcConfig extends WebMvcConfigurerAdapter {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
-
         registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
         registry.addResourceHandler("/webjars*//**").addResourceLocations("classpath:/META-INF/resources/webjars/");
     }
