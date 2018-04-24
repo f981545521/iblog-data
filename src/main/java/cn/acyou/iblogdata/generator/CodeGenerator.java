a1c4064da8f9c0f5bdc5f415d55ca14593118bb4
youfang <981545521@qq.com>
2018-04-24 19:45:39 +0800

    add : generator Mappers

---
+++ src/main/java/cn/acyou/iblogdata/generator/CodeGenerator.java
@@ -0,0 +1,32 @@
+package cn.acyou.iblogdata.generator;
+
+import org.mybatis.generator.api.MyBatisGenerator;
+import org.mybatis.generator.config.Configuration;
+import org.mybatis.generator.config.xml.ConfigurationParser;
+import org.mybatis.generator.internal.DefaultShellCallback;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.apache.ibatis.io.Resources.getResourceAsStream;
+
+/**
+ * @author youfang
+ * @date 2018-04-24 下午 06:24
+ **/
+public class CodeGenerator {
+
+    public static void main(String[] args) throws Exception {
+        List<String> warnings = new ArrayList<String>();
+        boolean overwrite = true;
+        ConfigurationParser cp = new ConfigurationParser(warnings);
+        Configuration config =
+                cp.parseConfiguration(getResourceAsStream("generator/generatorConfig.xml"));
+        DefaultShellCallback callback = new DefaultShellCallback(overwrite);
+        MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config, callback, warnings);
+        myBatisGenerator.generate(null);
+        for (String warning : warnings) {
+            System.out.println(warning);
+        }
+    }
+}
