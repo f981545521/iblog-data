edc017dde26fb94a5a9f73ec263ab17803bf03fe
youfang <981545521@qq.com>
2018-05-04 20:39:48 +0800

    add : bean util

---
+++ src/main/java/cn/acyou/iblogdata/utils/BeanUtil.java
@@ -0,0 +1,29 @@
+package cn.acyou.iblogdata.utils;
+
+import java.lang.reflect.Field;
+import java.util.HashMap;
+
+public class BeanUtil {
+    public static HashMap<String, Object> convertToMap(Object obj){
+        HashMap<String, Object> map = new HashMap<String, Object>();
+        Field[] fields = obj.getClass().getDeclaredFields();
+        for (int i = 0, len = fields.length; i < len; i++) {
+            String varName = fields[i].getName();
+            if (varName.equals("serialVersionUID")){
+                continue;
+            }
+            boolean accessFlag = fields[i].isAccessible();
+            fields[i].setAccessible(true);
+            Object o = null;
+            try {
+                o = fields[i].get(obj);
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            }
+            if (o != null)
+                map.put(varName, o.toString());
+            fields[i].setAccessible(accessFlag);
+        }
+        return map;
+    }
+}
