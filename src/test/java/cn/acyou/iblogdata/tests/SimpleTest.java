9efad5bdba00a5fc2cbd925110b6704d9ca0a5ed
f981545521 <981545521@qq.com>
2018-05-04 23:14:19 +0800

    add : Lombok

--- src/test/java/cn/acyou/iblogdata/tests/SimpleTest.java
+++ src/test/java/cn/acyou/iblogdata/tests/SimpleTest.java
@@ -3,6 +3,7 @@ package cn.acyou.iblogdata.tests;
 import cn.acyou.iblogdata.entity.Student;
 import cn.acyou.iblogdata.utils.BeanUtil;
 import org.junit.Test;
+import org.springframework.beans.BeanUtils;
 
 import java.util.Map;
 
@@ -10,10 +11,12 @@ public class SimpleTest {
     @Test
     public void test21(){
         Student student = new Student();
+        Student student2 = new Student();
         student.setId(1223);
         student.setName("张而非");
         student.setAge(null);
         Map<String, Object> map = BeanUtil.convertToMap(student);
-        System.out.println(map);
+        BeanUtils.copyProperties(student, student2);
+        System.out.println(student2);
     }
 }
