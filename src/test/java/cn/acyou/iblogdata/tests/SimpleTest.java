b74b5650c3c2b6022905ebbf3955a11b975f67b2
youfang <981545521@qq.com>
2018-05-19 11:04:51 +0800

    add : wx access

--- src/test/java/cn/acyou/iblogdata/tests/SimpleTest.java
+++ src/test/java/cn/acyou/iblogdata/tests/SimpleTest.java
@@ -9,6 +9,8 @@ import org.springframework.data.redis.core.script.DigestUtils;
 
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Map;
 
 public class SimpleTest {
@@ -45,8 +47,22 @@ public class SimpleTest {
 
     @Test
     public void testSHA1(){
-        String str = "youfang";
-        String ss = DigestUtils.sha1DigestAsHex(str);
-        System.out.println(ss);
+
+        String signature = "8a5b60eb577869ec8d33af0676461a527c57e28d";
+        String timestamp = "1526698659";
+        String nonce =  "1156985779";
+        String echostr =  "13059544329142392751";
+        String token =  "youfang";
+
+        ArrayList<String> list = new ArrayList<>();
+        list.add(nonce);
+        list.add(timestamp);
+        list.add(token);
+        //字典序排序
+        Collections.sort(list);
+        System.out.println(list.get(0) + list.get(1) + list.get(2));
+        String encodStr = DigestUtils.sha1DigestAsHex(list.get(0) + list.get(1) + list.get(2));
+        System.out.println(encodStr);
+        System.out.println(signature);
     }
 }
