4cc62f7ddce8e77e7b342d94213b35c1d4f19837
youfang <981545521@qq.com>
2018-06-09 11:41:17 +0800

    add : tests

--- src/test/java/cn/acyou/iblogdata/tests/SimpleTest.java
+++ src/test/java/cn/acyou/iblogdata/tests/SimpleTest.java
@@ -2,9 +2,11 @@ package cn.acyou.iblogdata.tests;
 
 import cn.acyou.iblogdata.entity.Student;
 import cn.acyou.iblogdata.utils.BeanUtil;
+import com.google.common.collect.Lists;
 import org.junit.Test;
 import org.springframework.beans.BeanUtils;
-import java.util.Map;
+
+import java.util.*;
 
 public class SimpleTest {
     @Test
@@ -30,6 +32,43 @@ public class SimpleTest {
         System.out.println(aa);
         System.out.println(ss.replaceAll("<", "&lt;"));
         System.out.println(ss.replaceAll(">", "&gt;"));
+    }
+
+
+
+    @Test
+    public void etst2(){
+        List<Map<String, String>> list = Lists.newArrayList();
+        Map<String, String> map = new HashMap<String, String>();
+        map.put("232","23");
+        map.put("233","23");
+        list.add(map);
+        Map<String, String> map2 = new HashMap<String, String>();
+        map2.put("232","fsf");
+        map2.put("235","23");
+        list.add(map2);
+        for (int i = 0; i < list.size(); i++){
+            Map<String, String> item = list.get(i);
+            if ("1".equals(item.get("code"))) {
+                list.remove(item);
+            }
+        }
+        Set<Map.Entry<String, String>> set =  map.entrySet();
+        Iterator<Map.Entry<String, String>> iterator2 = set.iterator();
+        while (iterator2.hasNext()) {
+            Map.Entry<String, String> item = iterator2.next();
+            item.getKey();
+        }
+
+        Iterator<Map<String, String>> iterator = list.iterator();
+        while (iterator.hasNext()) {
+            Map<String, String> item = iterator.next();
+            if ("fsf".equals(item.get("232"))) {
+                iterator.remove();
+            }
+        }
+        System.out.println(list);
+
     }
 
 }
