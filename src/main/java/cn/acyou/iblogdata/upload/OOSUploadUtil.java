3c06e02b724ec02463dea1b1827dc655e0f4f182
youfang <981545521@qq.com>
2018-06-20 15:16:09 +0800

    update : oos

--- src/main/java/cn/acyou/iblogdata/upload/OOSUploadUtil.java
+++ src/main/java/cn/acyou/iblogdata/upload/OOSUploadUtil.java
@@ -4,6 +4,7 @@ import com.aliyun.oss.OSSClient;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.util.UUID;
 
 /**
  * @author youfang
@@ -14,12 +15,14 @@ public class OOSUploadUtil {
         String fileName = "D:\\tmp\\256.mp4";
         FileInputStream inputStream = new FileInputStream(fileName);
         String endpoint = "http://oss-cn-beijing.aliyuncs.com";
-        String title = fileName.substring(fileName.lastIndexOf("\\") + 1);
+        //String title = fileName.substring(fileName.lastIndexOf("\\") + 1);
+        String title = UUID.randomUUID().toString() + fileName.substring(fileName.lastIndexOf("."));
         // 创建OSSClient实例。
         OSSClient ossClient = new OSSClient(endpoint, UploadConstant.ACCESS_KEY_ID, UploadConstant.ACCESS_KEY_SECRET);
         // 上传文件流。
         ossClient.putObject("ib-images", title, inputStream);
         // 关闭OSSClient。
         ossClient.shutdown();
+        System.out.println("https://ib-images.oss-cn-beijing.aliyuncs.com/"+ title);
     }
 }
