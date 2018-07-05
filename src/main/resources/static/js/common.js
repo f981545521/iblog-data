4de258e027b58b6ec14244c6e298bc7a74f24bb1
f981545521 <981545521@qq.com>
2018-07-05 22:13:30 +0800

    add : oss upload

--- src/main/resources/static/js/common.js
+++ src/main/resources/static/js/common.js
@@ -13,6 +13,35 @@ function ajaxPost(ajaxdata, ajaxurl, successcallback, errorcallback) {
         url: ajaxurl,
         data: ajaxdata,
         async: true,
+        success: function (data) {
+            if ($.isFunction(successcallback)) {
+                successcallback.call(this, data);
+            }
+        },
+        error: function (data) {
+            if ($.isFunction(errorcallback)) {
+                errorcallback.call(this, data);
+            }
+        }
+    });
+}
+
+/**
+ * processData设置为false。因为data值是FormData对象，不需要对数据做处理。
+ * <form>标签添加enctype="multipart/form-data"属性。
+ * cache设置为false，上传文件不需要缓存。
+ * contentType设置为false。因为是由<form>表单构造的FormData对象，且已经声明了属性enctype="multipart/form-data"，所以这里设置为false。
+ *
+ * ajax Formpost提交
+ */
+function ajaxFormPost(ajaxdata, ajaxurl, successcallback, errorcallback) {
+    $.ajax({
+        type: "post",
+        url: ajaxurl,
+        data: ajaxdata,
+        async: true,
+        dataType: "json",
+        contentType : false,
         processData: false,
         success: function (data) {
             if ($.isFunction(successcallback)) {
