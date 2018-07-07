006542bce92df4539bc86ae699a7eb30cb1ed6b4
youfang <981545521@qq.com>
2018-07-07 18:43:54 +0800

    add : RandomUtil

---
+++ src/main/java/cn/acyou/iblogdata/utils/RandomUtil.java
@@ -0,0 +1,61 @@
+package cn.acyou.iblogdata.utils;
+
+import java.util.UUID;
+
+/**
+ * @author youfang
+ * @version [1.0.0, 2018-07-07 下午 05:01]
+ **/
+public class RandomUtil {
+
+    /**
+     *
+     * @param numberFlag 是否数字
+     * @param length
+     * @return
+     */
+    public static String createRandom(boolean numberFlag, int length) {
+        StringBuilder retStr = new StringBuilder();
+        String strTable = numberFlag ? "1234567890"
+                : "1234567890abcdefghijkmnpqrstuvwxyz";
+        int len = strTable.length();
+        boolean bDone = true;
+        do {
+            retStr = new StringBuilder();
+            int count = 0;
+            for (int i = 0; i < length; i++) {
+                double dblR = Math.random() * len;
+                int intR = (int) Math.floor(dblR);
+                char c = strTable.charAt(intR);
+                if (('0' <= c) && (c <= '9')) {
+                    count++;
+                }
+                retStr.append(strTable.charAt(intR));
+            }
+            if (count >= 2) {
+                bDone = false;
+            }
+        } while (bDone);
+
+        return retStr.toString();
+    }
+
+    /**
+     * 递归生成指定长度的随机字符串
+     * @param length 长度
+     * @return 字符串
+     */
+    public static String createRandomStr(int length) {
+        String str = UUID.randomUUID().toString().replace("-","");
+        if (length < str.length()){
+            return str.substring(str.length() - length);
+        }
+        return str + createRandomStr(length - str.length());
+    }
+
+    public static void main(String[] args){
+        //System.out.println(UUID.randomUUID().toString().replace("-","").length());
+        String result = createRandomStr(180);
+        System.out.println(result + ":" + result.length());
+    }
+}
