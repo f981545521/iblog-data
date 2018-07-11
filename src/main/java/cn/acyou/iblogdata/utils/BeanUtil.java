0c81900ee853be72b535fc4a6436c5e4a11ce6bc
youfang <981545521@qq.com>
2018-07-11 23:37:18 +0800

    add : BeanUtil map to bean

--- src/main/java/cn/acyou/iblogdata/utils/BeanUtil.java
+++ src/main/java/cn/acyou/iblogdata/utils/BeanUtil.java
@@ -1,5 +1,7 @@
 package cn.acyou.iblogdata.utils;
 
+import com.alibaba.fastjson.JSON;
+
 import java.beans.BeanInfo;
 import java.beans.IntrospectionException;
 import java.beans.Introspector;
@@ -75,57 +77,17 @@ public class BeanUtil {
 
 
 
+
     /**
      * 将一个 Map 对象转化为一个 JavaBean
-     *
-     * @param type 要转化的类型
-     * @param map  包含属性值的 map
-     * @return 转化出来的 JavaBean 对象
-     * @throws IntrospectionException    如果分析类属性失败
-     * @throws IllegalAccessException    如果实例化 JavaBean 失败
-     * @throws InstantiationException    如果实例化 JavaBean 失败
-     * @throws InvocationTargetException 如果调用属性的 setter 方法失败
+     * @param clazz 对象所属类
+     * @param map Map
      */
     @SuppressWarnings("rawtypes")
-    public static Object convertMap(Class type, Map map) {
-        BeanInfo beanInfo = null; // 获取类属性
-        try {
-            beanInfo = Introspector.getBeanInfo(type);
-        } catch (IntrospectionException e) {
-            e.printStackTrace();
-        }
-        Object obj = null; // 创建 JavaBean 对象
-        try {
-            obj = type.newInstance();
-        } catch (InstantiationException e) {
-            e.printStackTrace();
-        } catch (IllegalAccessException e) {
-            e.printStackTrace();
-        }
-
-        // 给 JavaBean 对象的属性赋值
-        PropertyDescriptor[] propertyDescriptors = beanInfo.getPropertyDescriptors();
-        for (int i = 0; i < propertyDescriptors.length; i++) {
-            PropertyDescriptor descriptor = propertyDescriptors[i];
-            String propertyName = descriptor.getName();
-
-            if (map.containsKey(propertyName)) {
-                // 下面一句可以 try 起来，这样当一个属性赋值失败的时候就不会影响其他属性赋值。
-                Object value = map.get(propertyName);
-
-                Object[] args = new Object[1];
-                args[0] = value;
-
-                try {
-                    descriptor.getWriteMethod().invoke(obj, args);
-                } catch (IllegalAccessException e) {
-                    e.printStackTrace();
-                } catch (InvocationTargetException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-        return obj;
-
+    public static <T> T  convertMapToBean(Class<T> clazz, Map map) {
+        String jsonStr = JSON.toJSONString(map);
+        return JSON.parseObject(jsonStr, clazz);
     }
+
+
 }
