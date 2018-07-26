8964e9904034cf16c111cd2f382ad16301601ec4
youfang <981545521@qq.com>
2018-07-26 21:36:51 +0800

    add : 拦截器处理

--- src/main/java/cn/acyou/iblogdata/aop/SpringMvcInterceptor.java
+++ src/main/java/cn/acyou/iblogdata/aop/SpringMvcInterceptor.java
@@ -9,8 +9,11 @@
  */
 package cn.acyou.iblogdata.aop;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+import org.springframework.web.servlet.resource.ResourceHttpRequestHandler;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -20,6 +23,7 @@ import java.net.URLEncoder;
  * @author youfang
  * @version [1.0.0, 2018-07-26 下午 03:21]
  **/
+@Slf4j
 public class SpringMvcInterceptor extends HandlerInterceptorAdapter {
     @Value("${wx.wxCallback}")
     private String wxCallback;
@@ -29,8 +33,14 @@ public class SpringMvcInterceptor extends HandlerInterceptorAdapter {
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        String wxCallbackUrl = "https://open.weixin.qq.com/connect/oauth2/authorize" + "?appid="+appid+"&redirect_uri="+URLEncoder.encode(wxCallback, "UTF-8") +"&response_type=code&scope=snsapi_userinfo&state=1#wechat_redirect";
-        System.out.println("拦截器获取微信callback" + wxCallbackUrl);
+        if (handler instanceof HandlerMethod){
+            System.out.println("方法Handler");
+            String wxCallbackUrl = "https://open.weixin.qq.com/connect/oauth2/authorize" + "?appid="+appid+"&redirect_uri="+URLEncoder.encode(wxCallback, "UTF-8") +"&response_type=code&scope=snsapi_userinfo&state=1#wechat_redirect";
+            System.out.println("拦截器获取微信callback" + wxCallbackUrl);
+        }
+        if (handler instanceof ResourceHttpRequestHandler){
+            System.out.println("资源文件Handler");
+        }
         return super.preHandle(request, response, handler);
     }
 
--- src/main/java/cn/acyou/iblogdata/conf/WebMvcConfig.java
+++ src/main/java/cn/acyou/iblogdata/conf/WebMvcConfig.java
@@ -1,9 +1,15 @@
 package cn.acyou.iblogdata.conf;
 
 import cn.acyou.iblogdata.aop.SpringMvcInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.config.annotation.*;
+
+import java.util.Collections;
+import java.util.List;
 
 /**
  *
@@ -11,9 +17,18 @@ import org.springframework.web.servlet.config.annotation.*;
 @Configuration
 public class WebMvcConfig extends WebMvcConfigurerAdapter {
 
+    /**
+     * 根据类型注入一个List，只要Spring中有这个类型的都加到这个集合中
+     */
+    @Autowired(required = false)
+    private List<HandlerInterceptor> interceptorList = Collections.emptyList();
+
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(springMvcInterceptor()).addPathPatterns("/**");
+        //registry.addInterceptor(springMvcInterceptor()).addPathPatterns("/**");
+        for (HandlerInterceptor handlerInterceptor : interceptorList){
+            registry.addInterceptor(handlerInterceptor);
+        }
         super.addInterceptors(registry);
     }
 
@@ -29,9 +44,14 @@ public class WebMvcConfig extends WebMvcConfigurerAdapter {
     /**
      * springboot拦截器提取@Value属性值时为空的解决方案。
      * interceptor默认是不被spring context掌管的。所以还添加@bean ，加入的spring 容器下，才可以读取的spring容器内的变量值
-     * @return
+     * 通过Profile注解可以实现，除了某个环境加载Bean的问题
+     *
+     * Bean 的加载问题：
+     * Application启动后会自动扫描同级以及子级目录中的注解自动装备bean，需要加上@SpringBootApplication。
+     * 如果使用mybatis，在dao层接口上使用@Repository是扫描不出来的，需要使用@Mapper才行。
      */
     @Bean
+    @Profile("!dev")
     public SpringMvcInterceptor springMvcInterceptor() {
         return new SpringMvcInterceptor();
     }
