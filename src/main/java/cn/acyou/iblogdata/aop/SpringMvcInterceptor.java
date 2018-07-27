274133c17a7c9262d2da22928f1896328d03c5b3
youfang <981545521@qq.com>
2018-07-27 14:36:27 +0800

    add : read properties file with environment

--- src/main/java/cn/acyou/iblogdata/aop/SpringMvcInterceptor.java
+++ src/main/java/cn/acyou/iblogdata/aop/SpringMvcInterceptor.java
@@ -10,7 +10,9 @@
 package cn.acyou.iblogdata.aop;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 import org.springframework.web.servlet.resource.ResourceHttpRequestHandler;
@@ -31,9 +33,14 @@ public class SpringMvcInterceptor extends HandlerInterceptorAdapter {
     @Value("${wx.appid}")
     private String appid;
 
+    @Autowired
+    private Environment environment;
+
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         if (handler instanceof HandlerMethod){
+            String wxCallback = environment.getProperty("wx.wxCallback");//通过environment也可以获取properties中的配置文件
+            System.out.println(wxCallback);
             System.out.println("方法Handler");
             String wxCallbackUrl = "https://open.weixin.qq.com/connect/oauth2/authorize" + "?appid="+appid+"&redirect_uri="+URLEncoder.encode(wxCallback, "UTF-8") +"&response_type=code&scope=snsapi_userinfo&state=1#wechat_redirect";
             System.out.println("拦截器获取微信callback" + wxCallbackUrl);
