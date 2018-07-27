efa91630d8effdd179d259af78b258b4a653c245
youfang <981545521@qq.com>
2018-07-27 13:50:57 +0800

    add : thymeleaf execute Java code

---
+++ src/main/java/cn/acyou/iblogdata/utils/ThymeleafUtil.java
@@ -0,0 +1,13 @@
+package cn.acyou.iblogdata.utils;
+
+/**
+ * @author youfang
+ * @version [1.0.0, 2018-07-27 下午 01:28]
+ **/
+public class ThymeleafUtil {
+
+    public static String sayHello(){
+        return "你好";
+    }
+
+}
