b7ca100ddd5a1772e6ef62fa9457f12f23d2185b
youfang <981545521@qq.com>
2018-07-27 15:36:35 +0800

    add : rest template

---
+++ src/main/java/cn/acyou/iblogdata/conf/RestTemplateConfig.java
@@ -0,0 +1,52 @@
+package cn.acyou.iblogdata.conf;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.ClientHttpRequestFactory;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.web.client.RestTemplate;
+
+import java.nio.charset.Charset;
+import java.util.Iterator;
+import java.util.List;
+
+/**
+ * @author youfang
+ * @version [1.0.0, 2018-07-27 下午 02:58]
+ **/
+@Configuration
+public class RestTemplateConfig {
+    @Bean
+    public RestTemplate restTemplate(ClientHttpRequestFactory factory){
+        RestTemplate restTemplate = new RestTemplate(factory);
+
+        // 使用 utf-8 编码集的 conver 替换默认的 conver（默认的 string conver 的编码集为"ISO-8859-1"）
+        List<HttpMessageConverter<?>> messageConverters = restTemplate.getMessageConverters();
+        Iterator<HttpMessageConverter<?>> iterator = messageConverters.iterator();
+        while (iterator.hasNext()) {
+            HttpMessageConverter<?> converter = iterator.next();
+            if (converter instanceof StringHttpMessageConverter) {
+                iterator.remove();
+            }
+        }
+        messageConverters.add(new StringHttpMessageConverter(Charset.forName("UTF-8")));
+
+        return restTemplate;
+    }
+
+    /**
+     * 这个注解作用在bean定义上，它的作用就是在容器加载它作用的bean时，
+     * 检查容器中是否存在目标类型（ConditionalOnMissingBean注解的value值）的bean了，如果存在这跳过原始bean的BeanDefinition加载动作。
+     */
+    @Bean
+    @ConditionalOnMissingBean({ClientHttpRequestFactory.class})
+    public ClientHttpRequestFactory simpleClientHttpRequestFactory(){
+        SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
+        factory.setReadTimeout(5000);//单位为ms
+        factory.setConnectTimeout(5000);//单位为ms
+        return factory;
+    }
+}
