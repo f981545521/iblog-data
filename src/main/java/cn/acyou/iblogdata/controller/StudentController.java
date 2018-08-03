95c6f0e96a8a49b1b2fdb835126cd95089ad34f1
youfang <981545521@qq.com>
2018-08-03 12:01:42 +0800

    add : forward test

--- src/main/java/cn/acyou/iblogdata/controller/StudentController.java
+++ src/main/java/cn/acyou/iblogdata/controller/StudentController.java
@@ -15,7 +15,11 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.util.List;
 import java.util.Map;
 
@@ -122,6 +126,22 @@ public class StudentController {
     public ResultInfo getCondition(){
         List<Student> studentList = studentService.selectByCondition();
         return ResultInfoGenerate.generateSuccess(studentList);
+    }
+
+
+    @RequestMapping(value = "forward", method = {RequestMethod.GET})
+    @ApiOperation("Mapper Condition 查找")
+    @ResponseBody
+    public void forward(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        /*
+         * 1. 浏览器向Servlet1发出访问请求；
+         * 2. Servlet1调用forward()方法，在服务器端将请求转发给Servlet2；
+         * 3. 最终由Servlet2做出响应。
+         */
+        //获取请求转发器对象，该转发器的指向通过getRequestDisPatcher()的参数设置
+        RequestDispatcher requestDispatcher = request.getRequestDispatcher("/hello/doMethod");
+        //调用forward()方法，转发请求
+        requestDispatcher.forward(request, response);
     }
 
 
