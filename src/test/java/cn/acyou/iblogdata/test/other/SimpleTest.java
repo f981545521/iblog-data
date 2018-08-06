30d584044de5e9558dcc3c04738125780b778f15
youfang <981545521@qq.com>
2018-08-06 13:50:55 +0800

    add : Simple test case

---
+++ src/test/java/cn/acyou/iblogdata/test/other/SimpleTest.java
@@ -0,0 +1,16 @@
+package cn.acyou.iblogdata.test.other;
+
+import org.junit.Test;
+
+/**
+ * @author youfang
+ * @version [1.0.0, 2018-08-06 下午 01:49]
+ **/
+public class SimpleTest {
+
+    @Test
+    public void test1(){
+        Integer i1 = 1234;
+        System.out.println(i1.toString());
+    }
+}
