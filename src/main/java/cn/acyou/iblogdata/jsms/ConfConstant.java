eba0eb1b1871daedc484a2e9192a0b702998d05c
youfang <981545521@qq.com>
2018-08-09 16:06:14 +0800

    add : 极光短信集成

---
+++ src/main/java/cn/acyou/iblogdata/jsms/ConfConstant.java
@@ -0,0 +1,20 @@
+package cn.acyou.iblogdata.jsms;
+
+/**
+ * @author youfang
+ * @version [1.0.0, 2018-08-09 下午 02:50]
+ **/
+public class ConfConstant {
+
+    public static final String appkey = "d8d6f074c2535fb23f3192fa";
+    public static final String masterSecret = "1258965c645ccae4b3b37c81";
+
+    public static final String devKey = "5a5405f8fb7bd3a9cf3e3bf2";
+    public static final String devSecret = "1215eab9381c261542417de4";
+
+
+    public class JSMS{
+        public static final int template = 1;
+
+    }
+}
