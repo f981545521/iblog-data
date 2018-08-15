6fdae1a3a273ac42900812b84fd3547e2f372697
981545521 <981545521@qq.com>
2018-08-15 20:52:54 +0800

    add : post test

--- src/test/java/cn/acyou/iblogdata/test/tests/UtilTest.java
+++ src/test/java/cn/acyou/iblogdata/test/tests/UtilTest.java
@@ -1,9 +1,14 @@
 package cn.acyou.iblogdata.test.tests;
 
 import cn.acyou.iblogdata.test.base.BaseTest;
+import cn.acyou.iblogdata.utils.PostHelper;
 import cn.acyou.iblogdata.utils.RedisUtil;
+import cn.acyou.iblogdata.utils.ResultInfo;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * @author youfang
@@ -14,11 +19,23 @@ public class UtilTest extends BaseTest{
     @Autowired
     private RedisUtil redisUtil;
 
+    @Autowired
+    private PostHelper postHelper;
+
+    @Autowired
+    private HttpServletRequest request;
+
     @Test
     public void test1(){
         redisUtil.setValue("name", "小飞飞");
         System.out.println(redisUtil.containsKey("name"));
         System.out.println(redisUtil.getValue("name"));
+    }
+
+    @Test
+    public void test2(){
+        ResponseEntity<ResultInfo> resq =  postHelper.post(request, "http://localhost:8034/hello/paramStr", "123");
+        System.out.println(resq.getBody());
     }
 
 
