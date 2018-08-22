4847d9153e2546d62071a93b0195913d9b327927
youfang <981545521@qq.com>
2018-08-22 17:48:58 +0800

    add : improve code

--- src/main/java/cn/acyou/iblogdata/controller/UserController.java
+++ src/main/java/cn/acyou/iblogdata/controller/UserController.java
@@ -28,11 +28,6 @@ import java.util.List;
 @RequestMapping("/user")
 public class UserController extends BaseController{
 
-    @Autowired
-    private ThreadPoolTaskExecutor threadExecutor;
-
-    @Autowired
-    private JMessageHelper jMessageHelper;
 
     @RequestMapping(value = "login/{username}", method = {RequestMethod.GET})
     @ResponseBody
@@ -53,33 +48,6 @@ public class UserController extends BaseController{
     }
 
 
-    private void registerJiGuangAccount(final HttpServletRequest request, HttpSession session, String username){
 
-        //不能在多线程中直接使用request，request会在主线程完成后销毁。这里传递的是一个副本，无法通过它获取session
-        threadExecutor.execute(new Runnable() {
-
-            private ThreadLocal<HttpServletRequest> threadLocal = new ThreadLocal<HttpServletRequest>();
-
-            @Override
-            public void run() {
-                System.out.println("异步执行线程——————————————————> ");
-                threadLocal.set(request);
-                System.out.println("将要注册：" + username);
-                String secret = username + "xiaojuanniao";
-                String password = Md5Util.getMD5(secret);
-                System.out.println("密码：" + password);
-                List<RegisterInfo> users = new ArrayList<RegisterInfo>();
-                RegisterInfo user = RegisterInfo.newBuilder().setUsername(username).setPassword(password).build();
-                users.add(user);
-                jMessageHelper.registerUsers(request, users);
-                //HttpSession httpSession = request.getSession();
-                System.out.println("线程中的Session访问信息" + session.getAttribute("user"));
-                System.out.println("线程中的Request访问信息 ：" + request.getRequestURL());
-                HttpSession httpSession = threadLocal.get().getSession();
-                System.out.println("注册第三方账号成功" + httpSession.getAttribute("user"));
-
-            }
-        });
-    }
 
 }
