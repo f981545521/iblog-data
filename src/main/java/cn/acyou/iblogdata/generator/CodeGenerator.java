1e769d59b217c77632c971ddf612e0f9f518148b
youfang <981545521@qq.com>
2018-08-24 11:36:31 +0800

    add : generator config

--- src/main/java/cn/acyou/iblogdata/generator/CodeGenerator.java
+++ src/main/java/cn/acyou/iblogdata/generator/CodeGenerator.java
@@ -11,6 +11,10 @@ import java.util.List;
 import static org.apache.ibatis.io.Resources.getResourceAsStream;
 
 /**
+ * 使用教程：需要在generatorConfig.xml中配置：
+ * 1. 数据库连接信息
+ * 2. 要生成的表
+ *
  * @author youfang
  * @date 2018-04-24 下午 06:24
  **/
@@ -20,8 +24,7 @@ public class CodeGenerator {
         List<String> warnings = new ArrayList<String>();
         boolean overwrite = true;
         ConfigurationParser cp = new ConfigurationParser(warnings);
-        Configuration config =
-                cp.parseConfiguration(getResourceAsStream("generator/generatorConfig.xml"));
+        Configuration config = cp.parseConfiguration(getResourceAsStream("generator/generatorConfig.xml"));
         DefaultShellCallback callback = new DefaultShellCallback(overwrite);
         MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config, callback, warnings);
         myBatisGenerator.generate(null);
