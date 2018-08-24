0f107418acf391714c5e10715ae001589c19e755
youfang <981545521@qq.com>
2018-08-24 23:53:39 +0800

    add : aspectj support

--- src/main/java/cn/acyou/iblogdata/aop/LogInterceptor.java
+++ src/main/java/cn/acyou/iblogdata/aop/LogInterceptor.java
@@ -6,7 +6,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.*;
 import org.springframework.stereotype.Component;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * 针对Controller 作日志记录，输入与输出
@@ -28,6 +27,22 @@ import org.springframework.web.bind.annotation.ResponseBody;
  * 我们通常使用明确的类和方法名称，或是利用正则表达式定义所匹配的类和方法名称来指定这些切点。
  * 稍后会介绍切入点表达式。另外，有些AOP框架是允许我们创建动态的切点，可以根据运行时的决策（比如方法的参数值）来决定是否应用通知。
  *
+ *
+ * https://blog.csdn.net/zhengchao1991/article/details/53391244
+ *
+ *  切入点指示符用来指示切入点表达式目的，，在Spring AOP中目前只有执行方法这一个连接点，Spring AOP支持的AspectJ切入点指示符如下：
+ *    execution：用于匹配方法执行的连接点；
+ *    within：用于匹配指定类型内的方法执行；
+ *    this：用于匹配当前AOP代理对象类型的执行方法；注意是AOP代理对象的类型匹配，这样就可能包括引入接口也类型匹配；
+ *    target：用于匹配当前目标对象类型的执行方法；注意是目标对象的类型匹配，这样就不包括引入接口也类型匹配；
+ *    args：用于匹配当前执行的方法传入的参数为指定类型的执行方法；
+ *  - @within：用于匹配所以持有指定注解类型内的方法；
+ *  - @target：用于匹配当前目标对象类型的执行方法，其中目标对象持有指定的注解；
+ *  - @args：用于匹配当前执行的方法传入的参数持有指定注解的执行；
+ *  - @annotation：用于匹配当前执行方法持有指定注解的方法；
+ *    bean：Spring AOP扩展的，AspectJ没有对于指示符，用于匹配特定名称的Bean对象的执行方法；
+ *    reference pointcut：表示引用其他命名切入点，只有@ApectJ风格支持，Schema风格不支持。
+ *  AspectJ切入点支持的切入点指示符还有： call、get、set、preinitialization、staticinitialization、initialization、handler、adviceexecution、withincode、cflow、cflowbelow、if、@this、@withincode；但Spring AOP目前不支持这些指示符，使用这些指示符将抛出IllegalArgumentException异常。这些指示符Spring AOP可能会在以后进行扩展。
  * @author youfang
  * @version [1.0.0, 2018-08-24 下午 10:05]
  **/
