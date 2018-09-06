9f56ea584cb75312f2ed18881179d6a78e9139f2
981545521 <981545521@qq.com>
2018-09-06 23:28:17 +0800

    add : spring security remember me

--- src/main/java/cn/acyou/iblogdata/security/MemberInfoService.java
+++ src/main/java/cn/acyou/iblogdata/security/MemberInfoService.java
@@ -32,4 +32,5 @@ public class MemberInfoService implements UserDetailsService {
         }
         return null;
     }
+
 }
--- src/main/java/cn/acyou/iblogdata/security/SecurityConfig.java
+++ src/main/java/cn/acyou/iblogdata/security/SecurityConfig.java
@@ -2,6 +2,7 @@ package cn.acyou.iblogdata.security;
 
 import cn.acyou.iblogdata.security.MemberAuthProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -9,10 +10,17 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.RememberMeServices;
+import org.springframework.security.web.authentication.rememberme.JdbcTokenRepositoryImpl;
+import org.springframework.security.web.authentication.rememberme.PersistentTokenBasedRememberMeServices;
+import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
+
+import javax.sql.DataSource;
 
 /**
  * spring security
  * https://docs.spring.io/spring-security/site/docs/current/reference/html/jc.html
+ * https://docs.spring.io/spring-security/site/docs/current/guides/html5//helloworld-boot.html#creating-your-spring-security-configuration
  * @author youfang
  * @version [1.0.0, 2018-9-6 下午 08:53]
  **/
@@ -29,22 +37,32 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     private AuthenticationFailureHandler authenticationFailHander;
 
+    @Autowired
+    private DataSource dataSource;   //是在application.properites
+
+    @Autowired
+    private MemberInfoService memberInfoService;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 //.formLogin().loginPage("/login").loginProcessingUrl("/user/login").failureUrl("/login-error").permitAll()  //表单登录，permitAll()表示这个不需要验证 登录页面，登录失败页面
                 .formLogin().loginPage("/login").loginProcessingUrl("/login/form")
-                .successHandler(authenticationSuccessHandler)
-                .failureHandler(authenticationFailHander).permitAll()  //表单登录，permitAll()表示这个不需要验证 登录页面，登录失败页面
+                    .successHandler(authenticationSuccessHandler)
+                    .failureHandler(authenticationFailHander).permitAll()  //表单登录，permitAll()表示这个不需要验证 登录页面，登录失败页面
                 .and()
-                .authorizeRequests()
-                    .antMatchers("/dist/**", "/dist").permitAll()//允许所有用户都有权限访问登录页面
-                    .antMatchers("/h5plus/**", "/h5plus").permitAll()
-                    .antMatchers("/static/**", "/static").permitAll()
-                    .antMatchers("/student/students").hasRole("ADMIN") //表示这个资源需要有ROLE_ADMIN的这个角色才能访问。不然就会提示拒绝访问
-                    .anyRequest().authenticated() //任何没有匹配上的其他的url请求，只需要用户被验证
+                    .rememberMe()
+                        .tokenRepository(persistentTokenRepository())
+                        .tokenValiditySeconds(60)
                 .and()
-                .csrf().disable();
+                    .authorizeRequests()
+                        .antMatchers("/dist/**", "/dist").permitAll()//允许所有用户都有权限访问登录页面
+                        .antMatchers("/h5plus/**", "/h5plus").permitAll()
+                        .antMatchers("/static/**", "/static").permitAll()
+                        .antMatchers("/student/students").hasRole("ADMIN") //表示这个资源需要有ROLE_ADMIN的这个角色才能访问。不然就会提示拒绝访问
+                        .anyRequest().authenticated() //任何没有匹配上的其他的url请求，只需要用户被验证
+                .and()
+                    .csrf().disable();
     }
 
     @Autowired
@@ -52,6 +70,19 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         auth.authenticationProvider(authProvider);
 
     }
+
+
+    /**
+     * 记住我功能的token存取器配置
+     * @return
+     */
+    @Bean
+    public PersistentTokenRepository persistentTokenRepository() {
+        JdbcTokenRepositoryImpl tokenRepository = new JdbcTokenRepositoryImpl();
+        tokenRepository.setDataSource(dataSource);
+        return tokenRepository;
+    }
+
 
     /**
      * 第一个版本 （弃用）
