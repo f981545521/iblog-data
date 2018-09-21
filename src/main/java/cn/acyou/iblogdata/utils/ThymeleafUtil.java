e918675cc9c02e2b8441238d1f7999be9b2bb8c7
youfang <981545521@qq.com>
2018-09-21 12:29:22 +0800

    add : thymeleaf exec method

--- src/main/java/cn/acyou/iblogdata/utils/ThymeleafUtil.java
+++ src/main/java/cn/acyou/iblogdata/utils/ThymeleafUtil.java
@@ -1,9 +1,13 @@
 package cn.acyou.iblogdata.utils;
 
+import org.springframework.stereotype.Component;
+
 /**
+ * 使用@Component 注解，可以直接使用别名：${@thymeleafUtil.sayHelloTo(name)}
  * @author youfang
  * @version [1.0.0, 2018-07-27 下午 01:28]
  **/
+@Component(value = "thymeleafUtil")
 public class ThymeleafUtil {
 
     public static String sayHello(){
