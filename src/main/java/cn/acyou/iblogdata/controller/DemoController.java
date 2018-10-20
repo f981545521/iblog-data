8812e4d875bad3c54b9d75a43808b92e98429d40
youfang <981545521@qq.com>
2018-10-20 17:36:04 +0800

    add : easy poi export

--- src/main/java/cn/acyou/iblogdata/controller/DemoController.java
+++ src/main/java/cn/acyou/iblogdata/controller/DemoController.java
@@ -8,10 +8,12 @@ import cn.acyou.iblogdata.export.StudentEntityExportServer;
 import cn.acyou.iblogdata.utils.RandomUtil;
 import cn.afterturn.easypoi.entity.vo.BigExcelConstants;
 import cn.afterturn.easypoi.entity.vo.NormalExcelConstants;
+import cn.afterturn.easypoi.excel.ExcelExportUtil;
 import cn.afterturn.easypoi.excel.entity.ExportParams;
 import cn.afterturn.easypoi.excel.entity.enmus.ExcelType;
 import cn.afterturn.easypoi.view.PoiBaseView;
 import io.swagger.annotations.ApiOperation;
+import org.apache.poi.ss.usermodel.Workbook;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
@@ -23,6 +25,10 @@ import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
@@ -82,14 +88,14 @@ public class DemoController extends BaseController{
 
     @RequestMapping(value = "simpleExport", method = {RequestMethod.GET})
     @ApiOperation("easy poi 简单List导出")
-    public void simpleExport(ModelMap map, HttpServletResponse response) {
+    public void simpleExport(ModelMap map, HttpServletResponse response) throws IOException {
         List<StudentExportEntity> list = new ArrayList<>();
         for (int i = 0; i < 100; i++) {
             StudentExportEntity client = new StudentExportEntity();
             client.setId("2" + i);
             client.setName("小明" + i);
-            client.setBirthday(new Date());
-            client.setRegistrationDate(DateUtil.parseDate("2010-12-08"));
+            client.setBirthday(DateUtil.randomDate());
+            client.setRegistrationDate(DateUtil.parseDate("2010-12-08","yyyy-MM-dd"));
             client.setSex(RandomUtil.random01());
             StudentExportEntityGroup group = new StudentExportEntityGroup();
             group.setGroupName("测试" + i);
@@ -102,7 +108,15 @@ public class DemoController extends BaseController{
         map.put(NormalExcelConstants.CLASS, StudentExportEntity.class);//导出实体
         map.put(NormalExcelConstants.PARAMS, params);//参数
         map.put(NormalExcelConstants.FILE_NAME, "测试文件");//文件名称
-        PoiBaseView.render(map, request, response, NormalExcelConstants.EASYPOI_EXCEL_VIEW);
+
+        //方式1 ： View 返回
+        //PoiBaseView.render(map, request, response, NormalExcelConstants.EASYPOI_EXCEL_VIEW);
+        //方式2 ： 直接保存文件
+        Workbook workbook = ExcelExportUtil.exportExcel(params, StudentExportEntity.class, list );
+        FileOutputStream os = new FileOutputStream("F:\\iotest\\测试.xlsx");
+        workbook.write(os);
+        os.close();
+
         //return NormalExcelConstants.EASYPOI_EXCEL_VIEW;//View名称
     }
 
