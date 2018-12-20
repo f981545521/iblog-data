26cbe42000096db02dc1b4e0740808c99b6498a7
youfang <981545521@qq.com>
2018-12-20 17:33:15 +0800

    add : 测试单例模式线程安全性

--- src/main/java/cn/acyou/iblogdata/runner/MyCommandLineRunner.java
+++ src/main/java/cn/acyou/iblogdata/runner/MyCommandLineRunner.java
@@ -1,11 +1,14 @@
 package cn.acyou.iblogdata.runner;
 
 import cn.acyou.iblogdata.service.StudentService;
+import cn.acyou.iblogdata.utils.RedisUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Component;
+
+import java.util.concurrent.CountDownLatch;
 
 /**
  * Springboot给我们提供了两种“开机启动”某些方法的方式：ApplicationRunner和CommandLineRunner。
@@ -22,6 +25,9 @@ public class MyCommandLineRunner implements CommandLineRunner {
     @Autowired
     private ApplicationContext context;
 
+    @Autowired
+    private RedisUtil redisUtil;
+
     @Override
     public void run(String... var1) {
         log.info("MyCommandLineRunner was started!");
@@ -32,5 +38,34 @@ public class MyCommandLineRunner implements CommandLineRunner {
         //测试MyFactoryBean
         StudentService studentService = (StudentService) context.getBean("fbStudentService");
         studentService.sayThanks();
+
+        //testSingletonThread();
+    }
+
+    /**
+     * 测试单例模式线程安全性
+     */
+    public void testSingletonThread() {
+        //构造一个用给定计数初始化的 CountDownLatch。
+        final CountDownLatch latch = new CountDownLatch(1);
+        for (int i = 0; i < 10; i++) {
+            new Thread(() -> {
+                System.out.println("线程" + Thread.currentThread().getId() + "准备就绪");
+                try {
+                    latch.await();//使当前线程在锁存器倒计数至零之前一直等待，除非线程被中断。
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                redisUtil.print();
+                System.out.println("线程" + Thread.currentThread().getId() + "执行完成！");
+            }).start();
+        }
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        latch.countDown();//递减锁存器的计数，如果计数到达零，则释放所有等待的线程。
+        System.out.println("主线程完成！继续执行");
     }
 }
