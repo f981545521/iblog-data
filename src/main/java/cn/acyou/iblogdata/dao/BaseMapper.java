3d73887bfc8b71f21debdea4064e4ab51be15339
youfang <981545521@qq.com>
2018-12-21 16:53:12 +0800

    add : base mapper

---
+++ src/main/java/cn/acyou/iblogdata/dao/BaseMapper.java
@@ -0,0 +1,14 @@
+package cn.acyou.iblogdata.dao;
+
+/**
+ * @author youfang
+ * @version [1.0.0, 2018-12-21 下午 03:21]
+ **/
+public interface BaseMapper {
+
+    /**
+     * 生成UUID主键
+     * @return uuid
+     */
+    String getUUid();
+}
