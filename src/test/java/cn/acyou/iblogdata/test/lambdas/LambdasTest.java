90ff7aee57c6e09e477378ff95b2ce6fe6093b81
youfang <981545521@qq.com>
2019-01-11 13:56:27 +0800

    add : lambdas test

--- src/test/java/cn/acyou/iblogdata/test/lambdas/LambdasTest.java
+++ src/test/java/cn/acyou/iblogdata/test/lambdas/LambdasTest.java
@@ -3,11 +3,10 @@ package cn.acyou.iblogdata.test.lambdas;
 import cn.acyou.iblogdata.entity.Student;
 import cn.acyou.iblogdata.utils.RandomUtil;
 import org.apache.commons.lang3.math.NumberUtils;
+import org.junit.Before;
 import org.junit.Test;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Collectors;
 
 /**
@@ -15,10 +14,10 @@ import java.util.stream.Collectors;
  * @version [1.0.0, 2019-01-11 下午 01:09]
  **/
 public class LambdasTest {
+    List<Student> studentList = new ArrayList<>();
 
-    @Test
-    public void test1(){
-        List<Student> studentList = new ArrayList<>();
+    @Before
+    public void prepareData(){
         for (int i =1;i<10;i++){
             Student student = new Student();
             student.setId(i);
@@ -31,6 +30,10 @@ public class LambdasTest {
             System.out.println(student);
             studentList.add(student);
         }
+    }
+
+    @Test
+    public void test1(){
         List<Integer> ids = studentList.stream().map(Student::getId).collect(Collectors.toList());
         System.out.println(ids);
 
@@ -41,6 +44,26 @@ public class LambdasTest {
         Map<Integer, Student> studentMap = studentList.stream().collect(Collectors.toMap(Student::getId, x -> x));
         System.out.println(studentMap);
 
+    }
+
+    @Test
+    public void test2(){
+        // 分组： List里面的对象元素，以某个属性来分组，例如，以age分组，将age相同的放在一起：
+        Map<Integer, List<Student>> groupBy = studentList.stream().collect(Collectors.groupingBy(Student::getId));
+        //System.out.println(groupBy);
+
+        // 过滤filter: 从集合中过滤出来符合条件的元素 年龄>20的取出
+        List<Student> filterList = studentList.stream().filter(x -> x.getAge()>20).collect(Collectors.toList());
+
+        // 求和: 将集合中的数据按照某个属性求和 如：求年龄总和
+        int sum = studentList.stream().mapToInt(Student::getAge).sum();
+        System.out.println(sum);
+
+        // 查找流中最大 最小值
+        Optional<Student> minStudent = studentList.stream().min(Comparator.comparing(Student::getAge));
+        Optional<Student> maxStudent = studentList.stream().collect(Collectors.maxBy(Comparator.comparing(Student::getAge)));
+        minStudent.ifPresent(System.out::println);
+        maxStudent.ifPresent(System.out::println);
 
     }
 }
