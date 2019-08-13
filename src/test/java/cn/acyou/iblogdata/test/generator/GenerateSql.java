34bf8354e34435bdb59a900106bf875b6cfe1b42
youfang <981545521@qq.com>
2019-08-13 11:22:40 +0800

    add : 随机工具类

--- src/test/java/cn/acyou/iblogdata/test/generator/GenerateSql.java
+++ src/test/java/cn/acyou/iblogdata/test/generator/GenerateSql.java
@@ -27,18 +27,18 @@ public class GenerateSql {
             }
             String idNo = "";
             do {
-                idNo = RandomUtil.getIdNo(sex.equals("男"));
+                idNo = RandomUtil.randomIdCardNo(sex.equals("男"));
             } while (idNo.length() != 18);
 
             if (i%5000==0){
                 year ++;
             }
-            String yearData = year + "-" + String.format("%02d", RandomUtil.nextInt(12)) + "-" + String.format("%02d", RandomUtil.nextInt(28)) + " 00:00:00";
+            String yearData = year + "-" + RandomUtil.nextInt(12, 2) + "-" + String.format("%02d", RandomUtil.nextInt(28)) + " 00:00:00";
 
             String sql = "INSERT INTO `e_user` (`ID`, `IS_DELETE`, `CREATE_TIME`, `UPDATE_TIME`, `PASSWORD`, `STUDENT_NAME`, `SEX`, `CARD_CODE`, `ONE_CARD`, " +
                     "`PHONE`, `BIRTHDAY`, `ADDRESS`, `EMAIL`, `NATION`, `PHOTO_URL`, `USER_TYPE`, `BY_FACULTY_ID`, `BY_MAJOR_ID`, `BY_CLASS_ID`, `FACULTY_NAME`, `MAJOR_NAME`, `CLASS_NAME`, `CLASS_NO`, `STUDENT_CODE`, `ENTRANCE_DATE`, `POSITION`, `JOB_NUMBER`, `GRADE`, `USER_ROLE`, `V_CODE`)" +
                     " VALUES (DEFAULT, '0', '2019-08-08 00:00:00', '2019-08-08 00:00:00', '', '"+ RandomUtil.randomUserName() +"', '"+sexInt+"', '"+idNo+"', '"+RandomUtil.createRandom(true, 4)+
-                    "', '"+RandomUtil.getTelephone()+"', '2019-06-12 00:00:00', '南京', '211@qq.com', '汉族', 'http://192.168.1.188:8585/images/2019-06/1560492295352_121.png'," +
+                    "', '"+RandomUtil.randomTelephone()+"', '2019-06-12 00:00:00', '南京', '211@qq.com', '汉族', 'http://192.168.1.188:8585/images/2019-06/1560492295352_121.png'," +
                     " '1', '1', '0', '3', '信息系', '软件技术', '软件31班', '', '"+RandomUtil.createRandom(true, 4)+"', '"+yearData+"', '', '', '2', '0', '');";
 
             //String sql = "INSERT e_user (STUDENT_NAME,SEX,ONE_CODE,CARD_CODE,PHONE,NATION,FACULTY_NAME,MAJOR_NAME," +
