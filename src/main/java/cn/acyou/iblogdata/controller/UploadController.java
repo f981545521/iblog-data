dc479de756f540be86822240aded633f7971f4de
youfang <981545521@qq.com>
2020-04-10 20:08:26 +0800

    upload

--- src/main/java/cn/acyou/iblogdata/controller/UploadController.java
+++ src/main/java/cn/acyou/iblogdata/controller/UploadController.java
@@ -2,6 +2,7 @@ package cn.acyou.iblogdata.controller;
 
 import cn.acyou.iblogdata.annotation.WithoutLog;
 import cn.acyou.iblogdata.utils.ResultInfo;
+import cn.hutool.core.io.IoUtil;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
@@ -10,6 +11,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.io.FileOutputStream;
 
 /**
  * @author youfang
@@ -25,20 +28,25 @@ public class UploadController extends BaseController{
 
     /**
      * 上传一个文件
-     * @param file 文件
+     *
+     * @param file      文件
      * @param imageType @RequestParam(required = true)  Integer imageType时，不传这个参数会报错：
-     *                                             Required Integer parameter 'imageType' is not present
-     *                  @RequestParam(required = false) Integer imageType时，可以不传
+     *                  Required Integer parameter 'imageType' is not present
      * @return
+     * @RequestParam(required = false) Integer imageType时，可以不传
      */
     @PostMapping("image")
     @ApiOperation("上传一个文件")
-    public ResultInfo uploadimageFile(MultipartFile file, @RequestParam(required = false) Integer imageType){
-        log.info("上传图片类型：" + imageType + "，大小：" + file.getSize() );
+    public ResultInfo uploadimageFile(MultipartFile file, @RequestParam(required = false) Integer imageType) throws Exception {
+        log.info("上传图片类型：" + imageType + "，大小：" + file.getSize());
+        FileOutputStream fileOutputStream = new FileOutputStream("D://temp//test");
+        IoUtil.copy(file.getInputStream(), fileOutputStream);
         return new ResultInfo("上传成功");
     }
+
     /**
      * 上传一个文件
+     *
      * @param file 文件
      * @return
      */
